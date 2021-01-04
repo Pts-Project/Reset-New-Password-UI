af59d7d0c3b181d6c7d6d6badcee98207e8b1844
Trisha Sahu <55338588+Trisha-tech@users.noreply.github.com>
2021-01-04 17:54:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import "./App.css"
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
+import Profile from './components/screens/Profile'
+import ResetPassword from './components/screens/ResetPassword'
+import NewPassword from './components/screens/NewPassword'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Route exact path="/">
+    <Home/>
+    </Route>
+    <Route path="/signup">
+    <Signup/>
+    </Route>
+    <Route path="/login">
+    <Login/>
+    </Route>
+    <Route path="/profile">
+    <Profile/>
+    </Route>
+    <Route exact path="/changePassword">
+    <ResetPassword/>
+    </Route>
+    <Route path="/reset/:token">
+    <NewPassword/>
+    </Route>
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
